666267c6b3995423b7fbc34a5042ebd910af6033
Ashley <purpleizlegitx@gmail.com>
2020-06-28 14:32:13 +0100

    Implemented Log Item Implemented some log item logic, and added a preloader.

--- src/logs/Logs.js
+++ src/logs/Logs.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react'
+import LogItem from './LogItem'
+import Preloader from '../components/layout/Preloader'
 
  const Logs = () => {
 
@@ -19,15 +21,15 @@ import React, {useEffect, useState} from 'react'
     }
 
     if(loading) {
-        return <h4>Loading ...</h4>
+        return <Preloader/>
     }
 
     return (
-        <ul className="collection-with-header">
+        <ul className="collection with-header">
             <li className="collection-header">
                 <h4 className="center">System Logs</h4>
             </li>
-    {!loading && logs.length === 0 ? <p className="center">No Logs To Show</p> : logs.map(log => <li>{log.message}</li>)}
+    {!loading && logs.length === 0 ? <p className="center">No Logs To Show</p> : logs.map(log => <LogItem log={log} key={log.id}/>)}
         </ul>
     )
 }
