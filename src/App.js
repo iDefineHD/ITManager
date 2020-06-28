29e1445cde74de5535781fa7641627a22db7b28d
Ashley <purpleizlegitx@gmail.com>
2020-06-28 15:38:38 +0100

    Added initial modals Added the initial modals and set the toast system.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useEffect, Fragment} from 'react';
 import "materialize-css/dist/css/materialize.min.css"
 import M from 'materialize-css/dist/js/materialize.min.js'
 import SearchBar from './components/layout/AppSearchbar'
+import AddBtn from './components/layout/AddBtn'
+import AddLogModal from './logs/AddLogModal'
 import Logs from './logs/Logs'
 import './App.css';
 
@@ -14,6 +16,8 @@ const App = () => {
     <Fragment>
       <SearchBar/>
       <div className="container">
+        <AddBtn/>
+        <AddLogModal/>
         <Logs/>
       </div>
     </Fragment>
